fa7acfa02fbab36516da587b6fc8f898257498f7
Алексей Носаков <aleksnosakov@gmail.com>
2023-09-13 16:47:55 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title> Носаков Алексей Иванович, Лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy; Алексей Носаков, ФБи-13, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
