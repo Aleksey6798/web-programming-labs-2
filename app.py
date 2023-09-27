9cd9a0a8239f3e7ac721ee7c9059bd3699ed563a
Студент Факультета Бизнеса <student@FB.LOCAL>
2023-09-27 18:18:44 +0700

    Доделать дома

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def start():
     return redirect("/menu", code=302)
 
+@app.route("/ping")
+def s(): 
+    
 @app.route("/lab1")
 def lab1():
     return """
@@ -143,5 +146,18 @@ def example():
         {'name':'апельсины','price': 80},
         {'name':'мандарины','price': 95},
         {'name':'манго','price': 321}
+    ]
+    
+    books = [
+        {'namebook':'Убить пересмешника','author':'Харпер Ли', 'genre':'Роман','number_of_pages': 416},
+        {'namebook':'Гордость и предубеждение','author':'Джейн Остен', 'genre':'Роман','number_of_pages': 384},
+        {'namebook':'Дневник Анны Франк','author':'Анна Франк', 'genre':'Биография','number_of_pages': 296},
+        {'namebook':'1984','author':'Джордж Оруэлл', 'genre':'Научная фантастика','number_of_pages': 320},
+        {'namebook':'Гарри Поттер и философский камень','author':'Джоан Роулинг', 'genre':'Фэнтези','number_of_pages':3636},
+        {'namebook':'Властелин колец','author':'Дж.Р.Р.Толкин', 'genre':'','number_of_pages': ''},
+        {'namebook':'Великий Гэтсби','author':'Ф. С. Фицджеральд', 'genre':'','number_of_pages':''},
+        {'namebook':'Паутина Шарлотты','author':'Элвин Брукс Уайт', 'genre':'','number_of_pages':''},
+        {'namebook':'Маленькие женщины','author':'Луиза Мэй Олкотт', 'genre':'','number_of_pages':''},
+        {'namebook':'Хоббит','author':'Дж. Р. Р. Толкин', 'genre':'','number_of_pages':''}
     ]
     return render_template('example.html', name=name,numberlab=numberlab,numbercurs=numbercurs, group=group, fruits=fruits)
