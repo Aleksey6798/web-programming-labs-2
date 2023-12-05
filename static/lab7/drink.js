69250bfe415505fdf6ee4d5fde9321c25aaf5b6b
Носаков Алексей <aleksnosakov@gmail.com>
2023-12-05 15:35:35 +0700

    Возрат оплаты

--- static/lab7/drink.js
+++ static/lab7/drink.js
@@ -63,3 +63,41 @@ function pay() {
         document.querySelector('#price').innerHTML = 'Ошибка: ' + error.message;
     });
 }
+
+function cancel_pay() {
+    const drink = document.querySelector('[name=drink]:checked').value;
+    const milk = document.querySelector('[name=milk]').checked;
+    const sugar = document.querySelector('[name=sugar]').checked;
+    const card = document.querySelector('[name=card]').value;
+    const cvv = document.querySelector('[name=cvv]').value;
+
+    const obj = {
+        "method": "pay",
+        "params": {
+            drink: drink,
+            milk: milk,
+            sugar: sugar,
+            card: card,
+            cvv: cvv
+        }
+    };
+
+    fetch('/lab7/api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(obj)
+    })
+    .then(function(resp) {
+        return resp.json();
+    })
+    .then(function(data) {
+        if (data.error) {
+            document.querySelector('#pay-status').innerHTML = `Ошибка: ${data.error}`;
+        } else {
+            document.querySelector('#pay-status').innerHTML = 'Возврат средств прошло успешно на карту:' + card;
+            document.querySelector('#pay-status').style.display = 'block';
+        }
+    });
+}
