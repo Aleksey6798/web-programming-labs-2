ac728e36938c122df6ee145c1330b7760a3c0b75
Aleksey Nosakov <12345@.com>
2023-12-06 18:06:09 +0700

    Готовая лаба 7

--- static/lab7/drink.js
+++ static/lab7/drink.js
@@ -1,4 +1,4 @@
-function getPrice() {
+function get_price() {
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
     const drink = document.querySelector('[name=drink]:checked').value;
@@ -10,7 +10,6 @@ function getPrice() {
             milk: milk,
             sugar: sugar
         }
-
     };
 
     fetch('/lab7/api', {
@@ -21,84 +20,82 @@ function getPrice() {
         .then(function(resp) {
             return resp.json();
         })
-        .then(function(data) {
-            var drinkprice = 'Цена напитка:' + data.result + 'руб';
-            document.querySelector('#price').innerHTML = drinkprice;
+        .then(function(data){
+            document.querySelector('#price').innerHTML = `Цена напитка: ${data.result} руб`;
             document.querySelector('#pay').style.display = '';
+            document.querySelector('#refund').style.display = '';
         })
 }
 
 function pay() {
+    const cardNum = document.querySelector('[name=card]').value;
+    const cardName = document.querySelector('[name=name]').value;
+    const cvv = document.querySelector('[name=cvv]').value;
     const milk = document.querySelector('[name=milk]').checked;
     const sugar = document.querySelector('[name=sugar]').checked;
     const drink = document.querySelector('[name=drink]:checked').value;
-    const card_num = document.querySelector('[name=card_num]:cheched').value;
-    const cvv = document.querySelector('[name=cvv]:cheched').value;
 
     const obj = {
         "method": "pay",
         "params": {
+            card_num: cardNum,
+            card_name: cardName,
+            cvv: cvv,
             drink: drink,
             milk: milk,
-            sugar: sugar,
-            card_num: card_num,
-            cvv: cvv
+            sugar: sugar
         }
     };
 
     fetch('/lab7/api', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(obj)
+        })
+        .then(function(resp) {
+            return resp.json();
+        })
+        .then(function(data){
+            if (data.error) {
+                alert(data.error);
+            } else {
+                alert(data.result);
+            }
+        })
+}
+
+
+function refund() {
+    const cardNum = document.querySelector('[name=card]').value;
+    const cvv = document.querySelector('[name=cvv]').value;
+    const drink = document.querySelector('[name=drink]:checked').value;
+    const milk = document.querySelector('[name=milk]').checked;
+    const sugar = document.querySelector('[name=sugar]').checked;
+
+    const obj = {
+        "method": "refund",
+        "params": {
+            card_num: cardNum,
+            cvv: cvv,
+            drink: drink,
+            milk: milk,
+            sugar: sugar
+        }
+    };
+
+    fetch('/lab7/refund', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(obj)
     })
     .then(function(resp) {
-        if (!resp.ok) {
-            throw new Error('Ошибка' + resp.status);
-        }
         return resp.json();
     })
-    .then(function(data) {
-        document.querySelector('#price').innerHTML = 'Оплата прошла успешно: ' + data.result;
-    })
-    .catch(function(error) {
-        document.querySelector('#price').innerHTML = 'Ошибка: ' + error.message;
-    });
-}
-
-function cancel_pay() {
-    const drink = document.querySelector('[name=drink]:checked').value;
-    const milk = document.querySelector('[name=milk]').checked;
-    const sugar = document.querySelector('[name=sugar]').checked;
-    const card = document.querySelector('[name=card]').value;
-    const cvv = document.querySelector('[name=cvv]').value;
-
-    const obj = {
-        "method": "pay",
-        "params": {
-            drink: drink,
-            milk: milk,
-            sugar: sugar,
-            card: card,
-            cvv: cvv
-        }
-    };
-
-    fetch('/lab7/api', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(obj)
-    })
-    .then(function(resp) {
-        return resp.json();
-    })
-    .then(function(data) {
+    .then(function(data){
         if (data.error) {
-            document.querySelector('#pay-status').innerHTML = `Ошибка: ${data.error}`;
+            alert(data.error);
         } else {
-            document.querySelector('#pay-status').innerHTML = 'Возврат средств прошло успешно на карту:' + card;
-            document.querySelector('#pay-status').style.display = 'block';
+            alert(data.result);
         }
-    });
+    })
 }
