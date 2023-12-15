b35e6cbcf06021731c5c3de7cb84297d7c2044c1
Aleksey Nosakov <12345@.com>
2023-12-15 11:41:00 +0700

    Дата курса1

--- lab8.py
+++ lab8.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, abort, jsonify
+from datetime import datetime
 
 lab8 = Blueprint('lab8',__name__)
 
@@ -19,7 +20,7 @@ def get_courses():
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
 def get_course(course_num):
     if 0 <= course_num < len(courses):
-        return courses[course_num]
+        return jsonify(courses[course_num])
     else:
         abort(404)
 
@@ -28,19 +29,22 @@ def del_course(course_num):
     if 0 <= course_num < len(courses):
         del courses[course_num]
         return '', 204
+    else:
+        abort(404)
 
 @lab8.route('/lab8/api/courses/<int:course_num>', methods=['PUT'])
 def put_course(course_num):
-    course = request.get_json()
-    courses[course_num] = course
     if 0 <= course_num < len(courses):
-        return courses[course_num]
+        course = request.get_json()
+        course['createdAt'] = courses[course_num]['createdAt']
+        courses[course_num] = course
+        return jsonify(courses[course_num])
     else:
         abort(404)
-    return courses[course_num]
 
-@lab8.route('/lab8/api/courses/', methods=['GET'])
+@lab8.route('/lab8/api/courses/', methods=['POST'])
 def add_courses():
     course = request.get_json()
+    course['createdAt'] = datetime.now()
     courses.append(course)
     return {"num": len(courses)-1}
--- static/lab8/main.js
+++ static/lab8/main.js
@@ -1,56 +1,66 @@
 function fillCourseList() {
     fetch('/lab8/api/courses/')
-    .then(function (data){
-        return data.json();
-    })
-    .then(function (courses){
-        let tbody = document.getElementById('course-list');
-        tbody.innerHTML = '';
-        for(let i = 0; i<courses.length; i++){
-            tr = document.createElement('tr');
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (courses) {
+            let tbody = document.getElementById('course-list');
+            tbody.innerHTML = '';
+            for (let i = 0; i < courses.length; i++) {
+                let tr = document.createElement('tr');
 
-            let tdName = document.createElement('td');
-            tdName.innerText = courses[i].name;
-            
-            let tdVideos = document.createElement('td');
-            tdVideos.innerText = courses[i].videos;
-            
-            let tdPrice = document.createElement('td');
-            tdPrice.innerText = courses[i].price || 'бесплатно';
+                let tdName = document.createElement('td');
+                tdName.innerText = courses[i].name;
 
-            let editButton = document.createElement('button');
-            editButton.innerText = 'редактировать'
-            editButton.onclick = function() {
-                editCourse(i, courses[i]);
-            };
-            
-            let delButton = document.createElement('button');
-            delButton.innerText = 'удалить';
-            delButton.onclick = function() {
-                deleteCourse(i);
-            };
+                let tdVideos = document.createElement('td');
+                tdVideos.innerText = courses[i].videos;
 
-            let tdActions = document.createElement('td');
-            tdActions.append(editButton);
-            tdActions.append(delButton);
+                let tdPrice = document.createElement('td');
+                tdPrice.innerText = courses[i].price !== undefined ? courses[i].price : 'бесплатно';
 
-            tr.append(tdName);
-            tr.append(tdVideos);
-            tr.append(tdPrice);
-            tr.append(tdActions);
+                let tdDATA = document.createElement('td');
+                let serverDate = new Date(courses[i].createdAt);
+                let localDate = new Date(serverDate.getTime() + serverDate.getTimezoneOffset() * 60000);
+                tdDATA.innerText = localDate.toLocaleDateString();
 
-            tbody.append(tr);
-        }
-    })
+                let editButton = document.createElement('button');
+                editButton.innerText = 'редактировать';
+                editButton.onclick = function () {
+                    editCourse(i, courses[i]);
+                };
+
+                let delButton = document.createElement('button');
+                delButton.innerText = 'удалить';
+                delButton.onclick = function () {
+                    deleteCourse(i);
+                };
+
+                let tdActions = document.createElement('td');
+                tdActions.append(editButton);
+                tdActions.append(delButton);
+
+                tr.append(tdName);
+                tr.append(tdVideos);
+                tr.append(tdPrice);
+                tr.append(tdActions);
+                tr.append(tdDATA);
+                tbody.append(tr);
+            }
+        });
 }
 
 function deleteCourse(num) {
-    if(! confirm('Вы точно хотите удалить курс?'))
+    if (!confirm('Вы точно хотите удалить курс?')) {
         return;
-    fetch('/lab8/api/courses/${num}', {method:'DELETE'})
-    .then(function(){
-        fillCourseList();
-    });
+    }
+
+    fetch('/lab8/api/courses/' + num, { method: 'DELETE' })
+        .then(function () {
+            fillCourseList();
+        })
+        .catch(function (error) {
+            console.error('Ошибка при удалении курса:', error);
+        });
 }
 
 function showModal() {
@@ -65,6 +75,8 @@ function cancel() {
 }
 
 function addCourse() {
+    const course = {};
+    delete course.createdAt;
     document.getElementById('num').value = '';
     document.getElementById('name').value = '';
     document.getElementById('videos').value = '';
@@ -74,19 +86,31 @@ function addCourse() {
 
 function sendCourse() {
     const num = document.getElementById('num').value;
-    const course = {
-        name: document.getElementById('name').value,
-        videos: document.getElementById('videos').value,
-        price: document.getElementById('price').value,
+    const name = document.getElementById('name').value;
+    const videos = document.getElementById('videos').value;
+    const price = document.getElementById('price').value;
+    const normalizedPrice = price === '0' ? 'бесплатно' : price;
+
+    if (!name || !videos) {
+        alert('Заполните все поля!');
+        return;
     }
-    const url = '/lab8/api/courses/${num}';
-    const method = num? 'PUT' : 'POST';
+
+    const course = {
+        name: name,
+        videos: videos,
+        price: normalizedPrice,
+    };
+
+    const url = '/lab8/api/courses/' + num;
+    const method = num ? 'PUT' : 'POST';
+
     fetch(url, {
         method: method,
-        headers: {"Content-Type": "application/json"},
-        body: JSON.stringify(course)
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(course),
     })
-    .then(function(){
+    .then(function () {
         fillCourseList();
         hideModal();
     });
@@ -96,6 +120,7 @@ function editCourse(num, course) {
     document.getElementById('num').value = num;
     document.getElementById('name').value = course.name;
     document.getElementById('videos').value = course.videos;
-    document.getElementById('price').value = course.price;
+    document.getElementById('price').value = course.price === 'бесплатно' ? '0' : course.price;
+    delete course.createdAt;
     showModal();
 }
