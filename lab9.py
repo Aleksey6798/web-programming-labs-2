1524289eb749f2657d34072245c96782f7dcdd33
Aleksey Nosakov <12345@.com>
2023-12-19 14:59:32 +0700

    Обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -9,3 +9,12 @@ def main():
 @lab9.app_errorhandler(404)
 def not_found(e):
     return render_template('lab9/Error.html'), 404
+
+
+@lab9.route('/lab9/500')
+def server_error():
+    raise Exception("Произошла некоторая ошибка")
+
+@lab9.app_errorhandler(Exception)
+def er500(e):
+    return render_template('lab9/500.html', error=e), 500
